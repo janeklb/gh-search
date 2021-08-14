d06cdb971cac5c297463eddb4d974ec4bf6353cb
Janek Lasocki-Biczysko <janek.lasocki-biczysko@skyscanner.net>
2021-08-14 13:46:28 +0100

    Set per_page search request param to 100
    100 is the max value supported by GitHub's search api

--- ghsearch/client.py
+++ ghsearch/client.py
@@ -2,9 +2,11 @@ from typing import Any, Dict
 
 from github import Github
 
+RESULTS_PER_PAGE = 100  # this is the max - see https://docs.github.com/en/rest/reference/search#search-code--parameters
+
 
 def build_client(token: str, base_url: str = None) -> Github:
-    client_params: Dict[str, Any] = {"per_page": 1000, "login_or_token": token}
+    client_params: Dict[str, Any] = {"per_page": RESULTS_PER_PAGE, "login_or_token": token}
     if base_url:
         client_params["base_url"] = base_url
     return Github(**client_params)
