f5fefc565bf5298a60192fe6522a6814ae4fe3c4
Janek Lasocki-Biczysko <janek.lb@chainalysis.com>
2022-02-07 23:03:00 +0000

    chore: remove unnecessary setup parameters

--- setup.py
+++ setup.py
@@ -43,8 +43,6 @@ setup(
     long_description=get_readme(),
     long_description_content_type="text/markdown",
     packages=find_packages(exclude="tests"),
-    include_package_data=True,
-    platforms="any",
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
